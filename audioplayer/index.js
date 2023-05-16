20d8bea91d9bdc71e8871791a365a22835bc4c5d
electro9666 <electro9666@gmail.com>
2023-05-16 10:29:04 +0900

    no message

---
+++ audioplayer/index.js
@@ -0,0 +1,68 @@
+var $swiperWrapper = document.querySelector('.swiper-wrapper');
+_data.forEach((obj) => {
+  $swiperWrapper.innerHTML += `
+    <div class="swiper-slide" onclick="autoplay('resources/${obj.audio}')" data-audio="resources/${obj.audio}">
+      <img class="background" src="resources/${obj.img}">
+      <img class="center" src="resources/${obj.img}">
+    </div>
+  `;
+});
+
+var currentAudio = null;
+
+function fullscreenFn() {
+  document.documentElement.requestFullscreen()
+}
+
+
+function autoplay(filepath) {
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio = null;
+  }
+  var audio1 = new Audio(filepath);
+  currentAudio = audio1;
+  audio1.loop = false; // 반복재생하지 않음
+  audio1.volume = 1; // 음량 설정
+  audio1.play(); // sound1.mp3 재생
+}
+
+function audiostop() {
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio = null;
+  }
+}
+
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  // spaceBetween: 30,
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev",
+  // },
+  // slideToClickedSlide: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  effect: 'coverflow',
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 100,
+    depth: 150,
+    modifier: 1.5,
+    slideShadows: false,
+  },
+  on: {
+    slideChange: function () {
+      autoplay(this.slides[this.activeIndex].dataset.audio);
+    },
+  },
+});
+
+window.addEventListener('DOMContentLoaded', function () {
+
+});
