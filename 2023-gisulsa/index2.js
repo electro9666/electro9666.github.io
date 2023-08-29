5fc5ec1450efc994206838a897f3db5196b72e0e
electro9666 <electro9666@gmail.com>
2023-08-30 05:52:26 +0900

    no message

---
+++ 2023-gisulsa/index2.js
@@ -0,0 +1,177 @@
+var _items = [{ id: '관리131_1', text: `1. 디지털 트랜스포메이션(Digital Transformation)` },
+{ id: '관리131_1', text: `2. NFC(Near Field Communication)` },
+{ id: '관리131_1', text: `3. 폭포수 개발 방법론과 애자일 개발 방법론의 특징 및 장·단점 비교` },
+{ id: '관리131_1', text: `4. 클라우드 컴퓨팅의 Service Model과 Deployment Model 비교` },
+{ id: '관리131_1', text: `5. 데이터 차원 축소(Data Dimensionality Reduction)` },
+{ id: '관리131_1', text: `6. 정보시스템 감리와 PMO(Project Management Office) 비교` },
+{ id: '관리131_1', text: `7. 머신러닝(Machine Learning)과 딥러닝(Deep Learning) 차이` },
+{ id: '관리131_1', text: `8. 오토 스케일링(Auto Scaling)` },
+{ id: '관리131_1', text: `9. 독립표본 t-검정(Independent t-test)과 대응표본 t-검정(Paired t-test) 비교` },
+{ id: '관리131_1', text: `10. 크리덴셜 스터핑(Credential stuffing)` },
+{ id: '관리131_1', text: `11. 데이터 표준화의 필요성과 기대효과` },
+{ id: '관리131_1', text: `12. 객체지향 방법론에서 캡슐화(Encapsulation)와 정보은닉(Information Hiding)` },
+{ id: '관리131_1', text: `13. SBOM(Software Bill of Material)` },
+{ id: '관리131_2', text: `1. ISP(Information Strategic Planning)와 BPR(Business Process Reengineering)의 개념과 수행절차를 비교 설명하고, 기업에서 이 두 가지가 상호 보완적으로 활용하기 위한 방안을 설명하시오.` },
+{
+  id: '관리131_2', text: `2. 데이터 시각화(Data Visualization)와 관련하여 다음을 설명하시오.
+<br/>가. 데이터 시각화의 개요
+<br/>나. 데이터 시각화의 원리 및 절차
+<br/>다. 데이터 시각화 유형
+<br/>라. 효과적인 데이터 시각화를 위한 효율화 방안`},
+{ id: '관리131_2', text: `3. 인공지능의 개발 및 적용과정에서 윤리적으로 다루어져야 할 주요 내용과 인공지능을 효과적으로 관리하고 규제하기 위한 거버넌스 모형에 대하여 설명하시오.` },
+{ id: '관리131_2', text: `4. 제로 트러스트 보안(Zero Trust Security)모델의 보안원리, 핵심원칙, 적용분야를 트러스트 보안(Trust Security) 모델과 비교하여 설명하시오.` },
+{
+  id: '관리131_2', text: `5. 소켓(Socket) 통신과 관련하여 다음을 설명하시오.
+<br/>가. 소켓 통신 정의
+<br/>나. 소켓 통신 방식 개념도 및 유형
+<br/>다. TCP 소켓 및 Web 소켓 흐름 설명
+<br/>라. 소켓 통신 방식과 HTTP 통신 방식 비교`},
+{
+  id: '관리131_2', text: `6. 아키텍처 스타일과 디자인 패턴에 대하여 다음을 설명하시오.
+<br/>가. 아키텍처 스타일과 디자인 패턴의 차이점
+<br/>나. 대표적인 아키텍처 스타일 3가지
+<br/>다. GoF(Gang of Four) 디자인 패턴의 유형을 구분하고, 유형별 대표적인 디자인 패턴 설명`},
+{ id: '관리131_3', text: `1. 인공지능 학습용 데이터 허브 구축 과정에서 생성된 학습용 데이터 셋의 품질확보를 위한 주요활동과 데이터 생애 주기별 품질관리 수행절차에 대하여 설명하시오.` },
+{
+  id: '관리131_3', text: `2. 데이터 구조(Data Structure)에 대하여 다음을 설명하시오.
+<br/>가. 선형 구조(Linear Structure)의 개념 및 유형
+<br/>나. 비선형 구조(Non-Linear Structure)의 개념 및 유형
+<br/>다. 선형 구조(Linear Structure)와 비선형 구조(Non-Linear Structure) 비교`},
+{
+  id: '관리131_3', text: `3. 통합 테스트(Integration Test)에 대하여 다음을 설명하시오.
+<br/>가. 비점진적 통합 방식과 점진적 통합 방식
+<br/>나. 하향식(Top Down) 통합 테스트와 상향식(Bottom Up) 통합 테스트
+<br/>다. 테스트 드라이버(Test Driver)와 테스트 스텁(Test Stub)`},
+{
+  id: '관리131_3', text: `4. 소프트웨어 안전성 분석의 필요성과 다음의 분석 기법을 설명하시오.
+<br/>가. FTA(Fault Tree Analysis)
+<br/>나. FMEA(Failure Mode and Effects Analysis)
+<br/>다. HAZOP(Hazard and Operability Analysis)`},
+{ id: '관리131_3', text: `5. 운영체제 메모리 관리 기법 중 페이징 기법과 세그멘테이션 기법의 개념을 설명하고, 두 기법에 대하여 비교 설명하시오.` },
+{
+  id: '관리131_3', text: `6. 정보보호 및 개인정보보호 인증제도(ISMS, Information Security Management System)에 대하여 다음을 설명하시오.
+<br/>가. ISMS와 ISMS-P 차이점
+<br/>나. ISMS 의무 대상 기준`},
+{
+  id: '관리131_4', text: `1. 전략적 기업경영(Strategic Enterprise Management)에 대하여 다음을 설명하시오.
+<br/>가. 전략적 기업경영의 정의
+<br/>나. 전략적 기업경영의 구성요소
+<br/>다. 전략적 기업경영의 구축 방안 및 구축 절차`},
+{ id: '관리131_4', text: `2. 개인정보보호를 위한 ‘개인정보의 안전성 확보조치 기준’ 고시 내용 중 다음을 설명 하시오.<br/>가. 내부관리계획 수립·이행<br/>나. 암호화 적용방안` },
+{
+  id: '관리131_4', text: `3. 데이터 품질관리에 대하여 다음을 설명하시오.
+<br/>가. 데이터 품질관리 아키텍처
+<br/>나. 데이터 품질관리 성숙도
+<br/>다. 정형 데이터 및 비정형 데이터 품질기준
+<br/>라. 데이터 품질관리 전략`},
+{ id: '관리131_4', text: `4. 인공지능 분야에서 파운데이션(Foundation) 모델의 개념, 특징, 기반기술 및 구현시 법적·환경적·사회적 측면의 고려사항에 대하여 설명하시오.` },
+{
+  id: '관리131_4', text: `5. 소프트웨어 규모산정에 대하여 다음을 설명하시오.
+<br/>가. 필요성과 산정방법
+<br/>나. 규모산정 방식의 종류별 특징`},
+{
+  id: '관리131_4', text: `6. 정렬 알고리즘은 데이터Set이 주어졌을 때, 이를 사용자가 지정한 기준에 맞게 순서대로 나열하여 재배치하는 기법이다. 정렬 알고리즘과 관련하여 다음에 대하여 설명하시오.
+<br/>가. 버블 정렬
+<br/>나. 삽입 정렬
+<br/>다. 퀵 정렬`},
+{ id: '컴131_1', text: `1. 몬테 카를로 방법(Monte Carlo Method)` },
+{ id: '컴131_1', text: `2. NaaS(Network as a Service)` },
+{ id: '컴131_1', text: `3. 오토인코더(Autoencoder)` },
+{ id: '컴131_1', text: `4. 전이학습(Transfer Learning)` },
+{ id: '컴131_1', text: `5. ATAM(Architecture Trade-off Analysis Method)과 CBAM(Cost Benefit AnalysisMethod)` },
+{ id: '컴131_1', text: `6. PNM(Processing Near Memory)` },
+{ id: '컴131_1', text: `7. NoSQL의 CAP(Consistency, Availability, Partition Tolerance)` },
+{ id: '컴131_1', text: `8. RPA(Robotic Process Automation)` },
+{ id: '컴131_1', text: `9. 알고리즘의 시간복잡도(Time Complexity), 공간복잡도(Space Complexity)` },
+{ id: '컴131_1', text: `10. 지능정보화 기본법` },
+{ id: '컴131_1', text: `11. xAPI(eXperience Application Interface)` },
+{ id: '컴131_1', text: `12. CBPR(Cross Border Privacy Rule)` },
+{ id: '컴131_1', text: `13. CSRF(Cross-Site Request Forgery)` },
+{
+  id: '컴131_2', text: `1. 마르코프 특성(Markov Property)은 미래 상태의 조건부 확률분포가 과거상태와는독립적으로 현재 상태에 의해서만 결정된다는 것을 뜻한다. 이와 관련하여다음을설명하시오.
+<br/>가. 마르코프 결정 프로세스(Markov Decision Process)와 전이확률(Transition Probability)
+<br/>나. 상태가치함수(State Value Function)와 액션가치함수(State-Action Value Function)
+<br/>다. 벨만기대방정식(Bellman Expectation Equation)과 벨만최적방정식(Bellman Optimality Equation)`},
+{ id: '컴131_2', text: `2. 프로세스 스레싱(Thrashing)의 정의, 발생 원인과 해결방법을 설명하시오.` },
+{
+  id: '컴131_2', text: `3. 현행 데이터베이스 데이터량을 기준으로 TO-BE 데이터량을 예측하고자 한다. 이와 관련하여 다음을 설명하시오.
+<br/>가. 데이터베이스 용량산정 방법 별 개념 및 장·단점
+<br/>나. 데이터베이스 용량산정 기준`},
+{
+  id: '컴131_2', text: `4. 네트워크 자원을 보다 효율적으로 관리하고 최적화하기 위해, 네트워크 인프라에머신러닝 기법을 적용하고 있다. 이와 관련하여 다음을 설명하시오.
+<br/>가. 네트워크 인프라에 SDN(Software Defined Networking)을 이용하여 머신러닝기법을적용하는 이유
+<br/>나. SDN에서 강화학습 적용방안`},
+{
+  id: '컴131_2', text: `5. 칩렛(Chiplet)에 대하여 다음을 설명하시오.
+<br/>가. 칩렛의 개념
+<br/>나. 칩렛 구조의 장점
+<br/>다. 칩렛을 이어붙이는 방법`},
+{
+  id: '컴131_2', text: `6. 클라우드 서비스에 대하여 다음을 설명하시오.
+<br/>가. IT자원 유형에 따른 분류
+<br/>나. 서비스 개방 여부에 따른 분류
+<br/>다. MSP(Managed Service Provider)의 역할`},
+{
+  id: '컴131_3', text: `1. 현재의 딥러닝 기술은 사람의 눈으로 식별되지 않을 만큼 작은 노이즈를추가해서만든 적대적 예제(Adversarial Example)를 활용한 공격에 취약하다. 이와관련하여다음을 설명하시오.
+<br/>가. White-box 및 Black-box 적대적 공격에 대한 개념과 장·단점 비교
+<br/>나. 적대적 훈련(Adversarial Training) 및 Defense GAN(Generative Adversarial Networks) 방어기법`},
+{
+  id: '컴131_3', text: `2. 개인정보 비식별 처리와 관련하여 다음을 설명하시오.
+<br/>가. 개인정보 비식별 처리 유형
+<br/>나. 비식별 개인정보의 위험 요인`},
+{ id: '컴131_3', text: `3. 디스크 여러 개를 활용하여 속도를 높이고 안정성을 향상시키는기술인RAID(Redundant Array of Inexpensive Disk) 기술 중 RAID5와 RAID6에대하여설명하고, 최소 디스크 수량 및 고장 허용 측면에서 비교하여 설명하시오.` },
+{ id: '컴131_3', text: `4. 데이터베이스에 사용되는 트랜잭션의 개념과 이를 정의하는 4가지 중요한속성을가리키는 ACID의 각 요소에 대하여 설명하시오.` },
+{ id: '컴131_3', text: `5. 공공기관 정보화 사업 추진 시 국가정보원 보안성 검토 절차를 설명하시오.` },
+{ id: '컴131_3', text: `6. 데이터옵스(DataOps)의 주요 기술을 설명하고, 데브옵스(DevOps)와의 차이점을설명하시오.` },
+{
+  id: '컴131_4', text: `1. 강화학습(Reinforcement Learning)은 최적의 행동정책을 찾아가는 기계학습방법이다. 이와 관련하여 다음을 설명하시오.
+<br/>가. 가치기반 강화학습, 정책기반 강화학습, 액터 크리틱(Actor-Critic) 강화학습
+<br/>나. 정책경사(Policy Gradient) 방식 강화학습`},
+{
+  id: '컴131_4', text: `2. 추천시스템은 사용자의 과거 행동데이터 등을 바탕으로 사용자가 좋아할 만한정보나제품을 제시해 주는 시스템이다. 이와 관련하여 다음을 설명하시오.
+<br/>가. 컨텐츠 기반 필터링(Content-based Filtering)과 협업적 필터링(CollaborativeFiltering) 기법
+<br/>나. 행렬분해(Matrix Factorization) 기반 협업적 필터링`},
+{ id: '컴131_4', text: `3. 캐쉬 메모리(Cache Memory)에 대하여 다음을 설명하시오.<br/>가. 캐쉬 메모리의 개념과 구조<br/>나. 지역성(Locality)의 개념과 유형<br/>다. 캐쉬 일관성(Coherence) 문제의 원인과 해결 방법` },
+{
+  id: '컴131_4', text: `4. 스마트홈 연결 표준인 매터(Matter)에 대하여 다음을 설명하시오.
+<br/>가. 매터의 개념
+<br/>나. 기존 스마트홈 표준의 한계점과 매터의 장점
+<br/>다. 매터 Network Stack`},
+{ id: '컴131_4', text: `5. 시스템 성능 테스트 수행 시 성능 지표 관련하여 다음 물음에 답하시오.<br/>가. 응답시간(Response Time), 대기시간(Think Time), 동시사용자(Concurrent User), 활성사용자(Active User), TPS(Transaction Per Second)를 설명하시오.<br/>나. 동시사용자(Concurrent User)가 100명이고, 응답시간(Response Time)이5초이내이며, 대기시간(Think Time)이 15초인 시스템의 경우, TPS를 구하시오` },
+{
+  id: '컴131_4', text: `6. 공공기관 정보화 사업 추진 시 상용SW 직접구매 제도와 관련하여 다음을 설명하시오.
+<br/>가. 상용SW 직접구매 적용대상
+<br/>나. 상용SW 직접구매 예외기준
+<br/>다. 상용SW 직접구매와 일괄발주 비교`},]
+
+var _summaries = [{ text: `디지털 트랜스포메이션(Digital Transformation)` },
+{ text: `NFC(Near Field Communication)` },
+{ text: `폭포수 개발 방법론, 애자일 개발 방법론 > 특징, 장점, 단점, 비교` },
+{ text: `클라우드 컴퓨팅 > Service Model, Deployment Model > 비교` },
+{ text: `데이터 차원 축소(Data Dimensionality Reduction)` },
+{ text: `정보시스템 감리, PMO(Project Management Office) > 비교` },
+{ text: `머신러닝(Machine Learning), 딥러닝(Deep Learning) > 비교` },
+{ text: `오토 스케일링(Auto Scaling)` },
+{ text: `독립표본 t-검정(Independent t-test), 대응표본 t-검정(Paired t-test) > 비교` },
+{ text: `크리덴셜 스터핑(Credential stuffing)` },
+{ text: `데이터 표준화 > 필요성, 기대효과` },
+{ text: `객체지향 방법론 > 캡슐화(Encapsulation), 정보은닉(Information Hiding)` },
+{ text: `SBOM(Software Bill of Material)` },
+{ text: `ISP(Information Strategic Planning), BPR(Business Process Reengineering) > 개념, 수행절차, 기업활용방안` },
+{ text: `데이터 시각화(Data Visualization) > 개요, 원리, 절차, 유형, 효율화 방안` },
+{ text: `인공지능 개발, 적용 > 윤리, 관리, 규제 > 거버넌스 모형` },
+{ text: `제로 트러스트 보안(Zero Trust Security)모델, 트러스트 보안(Trust Security) 모델 > 보안원리, 핵심원칙, 적용분야 > 비교` },
+{ text: `소켓(Socket) 통신, HTTP 통신 > 정의, 방식 개념도, 유형, TCP 소켓, Web 소켓 흐름` },
+{ text: `아키텍처 스타일, 디자인 패턴 > 비교, 유형` },
+{ text: `인공지능 학습용 데이터 허브 구축 > 학습용 데이터 셋의 품질확보의 주요활동, 데이터 생애 주기별 품질관리 수행절차` },
+{ text: `데이터 구조(Data Structure) > 선형 구조(Linear Structure), 비선형 구조(Non-Linear Structure) > 개념, 유형, 비교` },
+{ text: `통합 테스트(Integration Test) > 비점진적 통합 방식, 점진적 통합 방식, 하향식(Top Down) 통합 테스트, 상향식(Bottom Up) 통합 테스트, 테스트 드라이버(Test Driver), 테스트 스텁(Test Stub)` },
+{ text: `소프트웨어 안전성 분석 > 필요성, 분석기법 > FTA(Fault Tree Analysis), FMEA(Failure Mode and Effects Analysis), HAZOP(Hazard and Operability Analysis)` },
+{ text: `운영체제 메모리 관리 기법 > 페이징 기법, 세그멘테이션 기법 > 개념, 비교` },
+{ text: `정보보호 및 개인정보보호 인증제도(ISMS, Information Security Management System) > ISMS와 ISMS-P 차이점, ISMS 의무 대상 기준` },
+{ text: `전략적 기업경영(Strategic Enterprise Management) > 정의, 구성요소, 구축 방안, 구축 절차` },
+{ text: `개인정보보호를 위한 '개인정보의 안전성 확보조치 기준' 고시 내용 > 내부관리계획 수립·이행, 암호화 적용방안` },
+{ text: `데이터 품질관리 > 아키텍처, 성숙도, 전략, 정형 데이터 및 비정형 데이터 품질기준` },
+{ text: `인공지능 > 파운데이션(Foundation) 모델 > 개념, 특징, 기반기술, 구현시 법적·환경적·사회적 측면의 고려사항` },
+{ text: `소프트웨어 규모산정 > 필요성, 산정방법, 규모산정 방식의 종류별 특징` },
+{ text: `정렬 알고리즘 > 버블 정렬, 삽입 정렬, 퀵 정렬` },]
